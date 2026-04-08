02dce14f0a21bc97184fbe25a4e517f643670230
Loïc <git@loicdugay.fr>
2026-04-08 15:45:25 +0200

    Refactor HeliosKwlButtonBase class structure

--- components/helios_kwl/button/helios_kwl_button.h
+++ components/helios_kwl/button/helios_kwl_button.h
@@ -1,36 +1,21 @@
 #pragma once
 #include "esphome/components/button/button.h"
 #include "../helios_kwl.h"
-
-namespace esphome {
-namespace helios_kwl {
-
+namespace esphome { namespace helios_kwl {
 class HeliosKwlButtonBase : public button::Button {
- public:
-  void set_parent(HeliosKwlComponent *parent) { parent_ = parent; }
- protected:
-  HeliosKwlComponent *parent_{nullptr};
+ public: void set_parent(HeliosKwlComponent *p) { parent_ = p; }
+ protected: HeliosKwlComponent *parent_{nullptr};
 };
-
 class HeliosKwlBoostAirflowButton : public HeliosKwlButtonBase {
- protected:
-  void press_action() override { if (parent_) parent_->trigger_boost_airflow(); }
+ protected: void press_action() override { if (parent_) parent_->trigger_boost_airflow(); }
 };
-
 class HeliosKwlBoostFireplaceButton : public HeliosKwlButtonBase {
- protected:
-  void press_action() override { if (parent_) parent_->trigger_boost_fireplace(); }
+ protected: void press_action() override { if (parent_) parent_->trigger_boost_fireplace(); }
 };
-
 class HeliosKwlStopBoostButton : public HeliosKwlButtonBase {
- protected:
-  void press_action() override { if (parent_) parent_->stop_boost_cycle(); }
+ protected: void press_action() override { if (parent_) parent_->stop_boost_cycle(); }
 };
-
 class HeliosKwlAckMaintenanceButton : public HeliosKwlButtonBase {
- protected:
-  void press_action() override { if (parent_) parent_->acknowledge_maintenance(); }
+ protected: void press_action() override { if (parent_) parent_->acknowledge_maintenance(); }
 };
-
-}  // namespace helios_kwl
-}  // namespace esphome
+} }
